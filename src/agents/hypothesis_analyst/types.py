ab6ef5911dab7735d14364a5180651da3673f514
nthday-jpg <24127166@student.hcmus.edu.vn>
2026-06-29 19:19:57 +0700

    fix

--- src/agents/hypothesis_analyst/types.py
+++ src/agents/hypothesis_analyst/types.py
@@ -1,2 +1,13 @@
-from ..types import AgentRequest, AgentResult
+from ..types import AgentRequest, AgentResult, BaseState
 
+class HypothesisAnalystRequest(AgentRequest):
+    ...
+
+class HypothesisAnalystResult(AgentResult):
+    ...
+
+class HypothesisAnalystState(BaseState):
+    ...
+
+
+    
