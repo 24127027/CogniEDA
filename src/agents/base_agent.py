98bd0b95cb4b4358335ca83d57d868e18b5578ec
nthday-jpg <24127166@student.hcmus.edu.vn>
2026-06-29 16:33:46 +0700

    abc

--- src/agents/base_agent.py
+++ src/agents/base_agent.py
@@ -3,33 +3,37 @@ from abc import ABC, abstractmethod
 
 from langgraph.graph.state import CompiledStateGraph
 
-from .types import AgentRequest, AgentResult
+from .types import AgentRequest, AgentResult, BaseState
 
 ReqT = TypeVar("ReqT", bound=AgentRequest)
 ResT = TypeVar("ResT", bound=AgentResult)
+StateT = TypeVar("StateT", bound=BaseState)
 
-class BaseAgent(ABC, Generic[ReqT, ResT]):
+class BaseAgent(ABC, Generic[ReqT, ResT, StateT]):
     def __init__(self, graph: CompiledStateGraph):
         self.graph = graph
 
     async def run(self, input: ReqT) -> ResT:
         """Run the agent with the given input and return the output."""
         
-        await self.before_run(input)
+        preprocessed_input = await self.before_run(input)
 
-        output = await self.graph.ainvoke(input)
+        output = await self.graph.ainvoke(preprocessed_input)
 
-        return await self.after_run(output)
-
-
-    async def before_run(self, input: ReqT):
-        """Hook to run before the main run method. Can be overridden by subclasses."""
-        pass
+        return await self.after_run(output) # type: ignore
 
     @abstractmethod
-    async def after_run(self, output: dict) -> ResT:
+    async def before_run(self, input: ReqT) -> StateT:
+        """
+        Preprocesses the input and prepares the state for the graph invocation.
+        This method can be overridden by subclasses to implement custom preprocessing logic.
+        """        
+        ...  
+
+    @abstractmethod
+    async def after_run(self, output: StateT) -> ResT:
         """
         Receives the final TypedDict state from LangGraph,
         extracts the final payload, and converts it to the expected ResT.
         """
-        pass
+        ...  
