834876fc73670e201ab2d051aefe89321778dbe2
nthday-jpg <24127166@student.hcmus.edu.vn>
2026-08-07 18:01:38 +0700

    temp fix for import

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-from src.agents import *
-
 __path__ = [str(Path(__file__).resolve().parent.parent / "src" / "agents")]
+
+from src.agents import *
--- tools/__init__.py
+++ tools/__init__.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
+__path__ = [str(Path(__file__).resolve().parent.parent / "src" / "tools")]
+
 from src.tools.builtin_tools import (
     AvailableBuiltinTools,
     BuiltinTool,
     create_dataset_toolset,
     create_graph_toolset,
 )
-
-__path__ = [str(Path(__file__).resolve().parent.parent / "src" / "tools")]
